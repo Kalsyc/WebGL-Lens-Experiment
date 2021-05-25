20bc04a8771029814652da5ae7a98a9cb5041da1
Darren Sim <klassiclegend@gmail.com>
2021-05-25 17:25:27 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
+/**
+ * Main script for WebGL (PixiJS) and Distortion/Blur Effect on Lenses (with Mouse/Touch input)
+ */
 var mainCanvasDiv,
   viewport,
-  clearSprite,
-  blurSprite,
-  focusSprite,
+  clearSprite, //Middle layer containing the clear background sprite
+  blurSprite, //Top layer containing the blurred and distorted background sprite
+  bgSprite, //Bottom layer containing the external blurred background sprite
   app,
   displace,
   circle,
   semicircle,
   ring;
 
+/**
+ * Initialize the main renderer with PixiJS and Pixi-Viewport plugin
+ */
 function initializeMainRenderer() {
   app = new PIXI.Application({
     width: window.innerWidth,
@@ -23,9 +29,10 @@ function initializeMainRenderer() {
     worldHeight: 500,
     interaction: app.renderer.plugins.interaction,
   });
-  viewport.wheel().pinch().decelerate();
+  viewport.wheel().pinch().decelerate(); //Attach wheel/pinch/decelerate functionalities (Can be removed);
   app.stage.addChild(viewport);
 
+  //Load images
   PIXI.Loader.shared
     .add([
       {
@@ -52,6 +59,9 @@ function initializeMainRenderer() {
     .load(setup);
 }
 
+/**
+ * Callback function to follow the ring around the user's touch
+ */
 function onMouseMove() {
   const pt = viewport.toLocal(
     app.renderer.plugins.interaction.eventData.data.global
@@ -67,24 +77,31 @@ function onMouseMove() {
   }
 }
 
+/**
+ * Instantiate sprite
+ * @param {string} name name of the image resource in PIXI.Loader
+ * @returns PIXI.Sprite object of image
+ */
 function instantiateSprite(name) {
   return new PIXI.Sprite(PIXI.Loader.shared.resources[name].texture);
 }
 
-function setupSprites() {
-  clearSprite = instantiateSprite("focus");
-  focusSprite = instantiateSprite("focus");
-  blurSprite = instantiateSprite("focus");
-}
-
-function setupRing() {
+/**
+ * Setup Ring around the user's touch
+ */
+function setupRing(scaleFactor = 0.8) {
   ring = instantiateSprite("ring");
   ring.anchor.set(0.5);
-  ring.scale.set(0.8);
+  ring.scale.set(scaleFactor);
   viewport.addChild(ring);
   ring.visible = false;
 }
 
+/**
+ * Setup Displacement map Filter
+ * @param {number} scaleX X-axis scale factor
+ * @param {number} scaleY Y-axis scale factor
+ */
 function setupDisplacement(scaleX = 70, scaleY = 0) {
   displace = instantiateSprite("displace");
   viewport.addChild(displace);
@@ -95,6 +112,9 @@ function setupDisplacement(scaleX = 70, scaleY = 0) {
   viewport.filters = [displacementFilter];
 }
 
+/**
+ * Setup Lens blur layer
+ */
 function setupLensBlur() {
   semicircle = instantiateSprite("lenses-all");
   semicircle.anchor.set(0.5);
@@ -102,21 +122,39 @@ function setupLensBlur() {
   blurSprite.mask = semicircle;
 }
 
+/**
+ * Setup Lens clear layer
+ */
 function setupLensClear() {
   circle = setupCircleMaskGraphics(ring ? ring.height / 2 : 60);
   clearSprite.mask = circle;
   viewport.addChild(circle);
 }
 
+/**
+ * Clamp/Restrict size to app screen width/height
+ * @param {PIXI.Sprite} sprite Sprite to be clamped on size
+ */
 function restrictSize(sprite) {
   sprite.height = app.screen.height / 2;
   sprite.width = app.screen.width / 2;
 }
 
+/**
+ * Setup blur filter
+ * @param {PIXI.Sprite} sprite PIXI.Sprite to be applied
+ * @param {number} strength Strength of blur filter
+ * @param {number} quality Quality/No. of passes of blur filter
+ */
 function setupBlurFiter(sprite, strength, quality) {
   sprite.filters = [new PIXI.filters.BlurFilter(strength, quality)];
 }
 
+/**
+ * Returns Circle Graphics for Masking
+ * @param {number} radius Radius of circle graphic
+ * @returns PIXI.Graphics object
+ */
 function setupCircleMaskGraphics(radius = 60) {
   return new PIXI.Graphics()
     .beginFill(0xff0000)
@@ -124,14 +162,19 @@ function setupCircleMaskGraphics(radius = 60) {
     .endFill();
 }
 
+/**
+ * Setup function
+ */
 function setup() {
-  setupSprites();
+  clearSprite = instantiateSprite("focus");
+  bgSprite = instantiateSprite("focus");
+  blurSprite = instantiateSprite("focus");
 
-  restrictSize(focusSprite);
+  restrictSize(bgSprite);
   restrictSize(clearSprite);
   restrictSize(blurSprite);
 
-  viewport.addChild(focusSprite);
+  viewport.addChild(bgSprite);
   viewport.addChild(clearSprite);
   viewport.addChild(blurSprite);
 
@@ -141,18 +184,18 @@ function setup() {
   setupLensBlur();
   setupLensClear();
 
-  setupBlurFiter(focusSprite, 20, 2);
+  setupBlurFiter(bgSprite, 20, 2);
   setupBlurFiter(blurSprite, 30, 10);
 
   //Configure viewport
 
-  viewport.zoomPercent(2);
+  viewport.zoomPercent(2); //Zoom 200%
 
   viewport.clamp({
     left: 0,
-    right: focusSprite.width,
+    right: bgSprite.width,
     top: 0,
-    bottom: focusSprite.height,
+    bottom: bgSprite.height,
   });
 
   viewport
